c2f7518f63df3ef983b75d514b1d3ab7c0c22f12
Russell Tan <sell_nat@yahoo.com>
2016-02-19 23:49:26 -0800

    Fixing appearance of location activity

--- MyApplication/app/src/main/java/com/example/russell_test/myapplication/LocationActivity.java
+++ MyApplication/app/src/main/java/com/example/russell_test/myapplication/LocationActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -16,6 +18,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class LocationActivity extends AppCompatActivity implements LocationListener {
     protected LocationManager locationManager;
@@ -34,7 +41,8 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
-        txtLat = (TextView) findViewById(R.id.textview1);
+        txtLat = (TextView) findViewById(R.id.currentLocation);
+        relativeAddress = (TextView) findViewById(R.id.relativeAddress);
         simLogin = (Button) findViewById(R.id.simulatedLogin);
         loginSuccess = new Intent(this, LobbyActivity.class);
 
@@ -66,21 +74,70 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         txtLat = (TextView) findViewById(R.id.currentLocation);
         txtLat.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
 
-        
+        Address relativeAddr = getAddress(location);
+
+        relativeAddress.setText(relativeAddr.getAddressLine(0));
+
+
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-        Log.d("Latitude","disable");
+        Log.d("Latitude", "disable");
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-        Log.d("Latitude","enable");
+        Log.d("Latitude", "enable");
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
         Log.d("Latitude","status");
+    }
+
+    private Address getAddress(Location location)
+    {
+        Address address = null;
+        double lat = location.getLatitude();
+        double longt = location.getLongitude();
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        try
+        {
+            List<Address> listAddresses = geocoder.getFromLocation(lat, longt, 1);
+            if(listAddresses.size() > 0)
+            {
+                address = listAddresses.get(0);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return address;
+    }
+
+    private Location getMidLocation(ArrayList<Location> locations)
+    {
+        Location midLocation = new Location("");
+
+        double sumX = 0;
+        double sumY = 0;
+
+        int k = 0;
+        int locCount = locations.size();
+
+        for(k = 0; k < locCount; k++)
+        {
+            sumX += locations.get(k).getLatitude();
+            sumY += locations.get(k).getLongitude();
+        }
+
+        midLocation.setLatitude( sumX / locCount);
+        midLocation.setLongitude(sumY / locCount);
+
+        return midLocation;
     }
 }
